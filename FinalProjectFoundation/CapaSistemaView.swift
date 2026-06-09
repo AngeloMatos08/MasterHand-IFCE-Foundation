dc53738ac2d1946b0f35f53f2488b7e3e42e08ba
Henk <henknarciso@gmail.com>
2026-06-09 13:51:59 -0300

    View da Capa do sistema

---
+++ FinalProjectFoundation/CapaSistemaView.swift
@@ -0,0 +1,18 @@
+//
+//  CapaSistemaView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 02/06/26.
+//
+
+import SwiftUI
+
+struct CapaSistemaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CapaSistemaView()
+}
